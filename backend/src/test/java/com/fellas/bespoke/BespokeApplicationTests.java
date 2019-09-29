81e682f091e146a1ea25543a5b6df0841a9ce90c
Cagri Altug <cagri.altug@siemens.com>
2019-09-29 14:40:33 +0300

    Create BespokeApplicationTests.java

---
+++ backend/src/test/java/com/fellas/bespoke/BespokeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fellas.bespoke;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BespokeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
