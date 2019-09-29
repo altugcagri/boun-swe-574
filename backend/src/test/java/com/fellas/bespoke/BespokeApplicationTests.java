57c40324537efe638a48b3e737cf94a6cd8c621c
Cagri Altug <cagri.altug@siemens.com>
2019-09-29 15:15:59 +0300

    h2 db

--- backend/src/test/java/com/fellas/bespoke/BespokeApplicationTests.java
+++ backend/src/test/java/com/fellas/bespoke/BespokeApplicationTests.java
@@ -3,9 +3,11 @@ package com.fellas.bespoke;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
+@PropertySource("classpath:application-test.properties")
 @SpringBootTest
 public class BespokeApplicationTests {
 
