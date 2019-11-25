8ab99567c6cdd2f9d0547a718da9b57bb8236c3f
Gulsah Coskun <gulsah.coskun@betbull.com>
2019-11-25 12:47:22 +0300

    get annotation null check and sonar fixes added

--- backend/src/main/java/com/fellas/bespoke/client/request/AnnotationBody.java
+++ backend/src/main/java/com/fellas/bespoke/client/request/AnnotationBody.java
@@ -2,12 +2,14 @@ package com.fellas.bespoke.client.request;
 
 import lombok.*;
 
+import java.io.Serializable;
+
 @Getter
 @Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class AnnotationBody {
+public class AnnotationBody implements Serializable {
 
     private String type;
     private String value;
--- backend/src/main/java/com/fellas/bespoke/client/request/AnnotationSelector.java
+++ backend/src/main/java/com/fellas/bespoke/client/request/AnnotationSelector.java
@@ -2,12 +2,14 @@ package com.fellas.bespoke.client.request;
 
 import lombok.*;
 
+import java.io.Serializable;
+
 @Getter
 @Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class AnnotationSelector {
+public class AnnotationSelector implements Serializable {
 
     private String type;
     private String value;
